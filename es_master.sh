385d6b465d706c6363fd0a1300126dd2ba6c8971
xiongwenhui <591566564@qq.com>
2019-10-17 16:08:25 +0800

    Update es_master.sh

--- es_master.sh
+++ es_master.sh
@@ -6,13 +6,13 @@ NodeMaster=true
 NodeData=false
 
 #安装jdk
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.rpm"
-yum install  jdk-8u141-linux-x64.rpm -y
-echo 'export JAVA_HOME=/usr/java/jdk1.8.0_141' >> /etc/profile
-echo 'export CLASSPATH=.:$CLASSPTAH:$JAVA_HOME/lib' >> /etc/profile
-echo 'export PATH=$PATH:$JAVA_HOME/bin' >> /etc/profile
-source /etc/profile
-java -version
+#wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.rpm"
+#yum install  jdk-8u141-linux-x64.rpm -y
+#echo 'export JAVA_HOME=/usr/java/jdk1.8.0_141' >> /etc/profile
+#echo 'export CLASSPATH=.:$CLASSPTAH:$JAVA_HOME/lib' >> /etc/profile
+#echo 'export PATH=$PATH:$JAVA_HOME/bin' >> /etc/profile
+#source /etc/profile
+#java -version
 
 #下载es
 wget --no-check-certificate https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.8.1.tar.gz
